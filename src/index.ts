634fc2c03ecab0cf256bc43484253cad63637053
Dominik Koch <dominik@koch-bautechnik.de>
2025-04-18 23:38:17 +0200

    chore: add auth route

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import "dotenv/config";
-
+import { auth } from "./lib/auth.js";
 const app = new Hono();
 
 app.get("/ping", (c) => {
   return c.json("pong!");
 });
+
+app.on(["POST", "GET"], "/auth/**", (c) => auth.handler(c.req.raw));
 
 serve(
   {
---
+++ src/lib/auth.ts
@@ -0,0 +1,29 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "../generated/prisma/client.js";
+
+const prisma = new PrismaClient();
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!, 
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!, 
+    },
+    apple: {
+      clientId: process.env.APPLE_CLIENT_ID!, 
+      clientSecret: process.env.APPLE_CLIENT_SECRET!, 
+      //appBundleIdentifier: process.env.APPLE_APP_BUNDLE_IDENTIFIER!, 
+    },
+    facebook: {
+      clientId: process.env.FACEBOOK_CLIENT_ID!, 
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!, 
+    },
+  },
+});
