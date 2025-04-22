d14a83363117919405fab6f89c764462933a4f24
Dominik Koch <dominik@koch-bautechnik.de>
2025-04-22 19:42:40 +0200

    refactor: add api versioning

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,20 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import "dotenv/config";
 import { auth } from "./lib/auth.js";
-const app = new Hono();
 
-app.get("/ping", (c) => {
+const app = new Hono();
+const v1 = new Hono();
+
+v1.get("/ping", (c) => {
   return c.json("pong!");
 });
 
-
-app.on(["POST", "GET"], "api/auth/**", (c) => {
+v1.on(["POST", "GET"], "api/auth/**", (c) => {
   console.log(`[${new Date().toISOString()}] ${c.req.method} ${c.req.url}`);
   return auth.handler(c.req.raw);
 });
 
+app.route("/v1", v1);
 
 serve(
   {
